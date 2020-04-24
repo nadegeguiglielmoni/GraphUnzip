e93fd05e598fc01f15fc83155f0d13f3c52ca98f
Roland <zsoupic@gmx.com>
2020-04-24 15:20:05 +0200

    -a

---
+++ solve_ambiguities.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 15:30:45 2020
+
+@author: zaltabar
+
+In this file we will use a lot of "supercontigs", meaning an assembly of several contigs. The syntax is a list
+of end of contigs, e.g. [40,41, 104,105,523,522] (and not [20,52,261])
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import basic_functions as bf
+
+def how_far_away_are_those_contigs(contig1, contig2, links, infContigs):
+    connectedToContig1 = [contig1*2, contig1*2+1]
+    
+    distanceToContig1 = [0,0]
+    
+    pathToContig1 = [[],[]]
+    
+    pastLengthOfconnexion1 = 1
+    
+    while pastLengthOfconnexion1 < len(connectedToContig1) :
+        
+        pastLengthOfconnexion1 = len(connectedToContig1)
+        newConnectedToContig1 = connectedToContig1[:]
+        
+        #we spread one step from contig1
+        for cc, connectedContig in enumerate(connectedToContig1) : #that can be optimized if we need speed, since we go through the first contigs many times
+            
+            if cc < 2 or cc%2 == 1 : #that's to move only one way in the contig graph
+                for newContig in links[connectedContig] :
+                            
+                    newdist = distanceToContig1[cc]+infContigs[int(connectedContig/2)][1]
+                    if newContig in newConnectedToContig1 :
+                        
+                        oldpos = newConnectedToContig1.index(newContig)
+                        
+                        if newdist < distanceToContig1[oldpos] :
+                            distanceToContig1[oldpos] = newdist
+                            pathToContig1[oldpos] = pathToContig1[cc]+[cc]
+                            
+                            distanceToContig1[oldpos+1-2*(oldpos%2)] = newdist
+                            pathToContig1[oldpos+1-2*(oldpos%2)] = pathToContig1[cc]+[cc]
+                    else :
+                        #we add both end of the contig to the connected contig
+                        newConnectedToContig1 += [newContig]
+                        distanceToContig1 += [newdist]
+                        pathToContig1 += [pathToContig1[cc]+[cc]]
+                        
+                        newConnectedToContig1 += [newContig + 1 - 2*(newContig%2)]
+                        distanceToContig1 += [newdist]
+                        pathToContig1 += [pathToContig1[cc]+[connectedContig]]
+        connectedToContig1 = newConnectedToContig1[:]
+        
+    #now we see where contig2 stands with respect to contig1
+    if contig2*2 in connectedToContig1 :
+        print(connectedToContig1)
+        indexContig2 = connectedToContig1.index(contig2*2)
+        return distanceToContig1[indexContig2]-infContigs[contig1][1], pathToContig1[indexContig2][1:] #because the length of contig 1 is always taken into account in the path length while it shouldn't
+    else :
+        print(connectedToContig1)
+        return -1 #meaning contig1 and contig2 are not connected in this graph
+
+#this function measures the intensity of interactions between one supercontig and several candidate, including without taking account of the common parts of the supercontigs
+def intensity_of_interactions(supercontig, listOfSuperContigs, interactionMatrix) :
+    commonContigs = []
+    for contig in listOfSuperContigs[0] :
+            common = True
+            for sg in listOfSuperContigs[1:] :
+                common = common and (contig in sg)
+            if common :
+                commonContigs += [contig]
+    #now we have a list of common contigs
+    
+    #we return for each supercontig its absolute score and its relative score (wihtout the common parts)
+    absoluteScore = [0 for i in range(len(listOfSuperContigs))]
+    relativeScore = [0 for i in range(len(listOfSuperContigs))]
+    
+    for sg in range(len(listOfSuperContigs)) :
+        for c in listOfSuperContigs[sg] :
+            for contig in supercontig :
+                if c%2 == 0 and contig%2 == 0  :#to count each interaction only once
+                    if c not in commonContigs :
+                        absoluteScore[sg] += interactionMatrix[int(c/2)][int(contig/2)]
+                        relativeScore[sg] += interactionMatrix[int(c/2)][int(contig/2)]
+                    else :
+                        absoluteScore[sg] += interactionMatrix[int(c/2)][int(contig/2)]
+    
+    return absoluteScore, relativeScore
+    
+#we're going to look specifically at one contig and its immediate surroundings
+def solve_ambiguity_around_this_contig(contig, links, hiccontactfile, fragmentList, infContigs):
+         return 0
+
+#links = bf.import_links('listsPython/links.csv')
+#infContigs = bf.read_info_contig('data/results/info_contigs.txt')
+interactionMatrix = bf.import_from_csv('listsPython/interactionMatrix.csv')
+
+print('coucou')
+print(intensity_of_interactions([246,247], [[1716, 1717], [1978,1979]], interactionMatrix))
+print('Finished')
+
+    
---
+++ transform_gfa.py
@@ -0,0 +1,148 @@
+# -*- coding: latin-1 -*-
+
+import time
+#from analyse_HiC import lookForStrings
+#print(r[:100000])
+
+
+def gfa_to_fasta() :
+    
+    gfa_read = open('data/Assembly.gfa')
+    r = gfa_read.read()
+
+    seqs = []
+    current = ''
+    count = 0
+    bases = ['A','C','G','T']
+    
+    t1 = time.time()
+    
+    for i in range(len(r)) :
+        count += 1
+        if current == '' :
+            if r[i] in bases and r[i+1] in bases and r[i+2] in bases and r[i+3] in bases :
+                current = r[i]
+    
+        else :
+            #if i%4 == 0 :
+            if r[i] in bases :
+                current += r[i]
+            else :
+                seqs += [current]
+                current = ''
+            #else :
+            #   current += r[i]
+        
+        if count % 1000000 == 0 :
+            print (count / 1000000)
+        
+        if count == 123533473 :
+            #print(current)
+            break
+                    
+    fasta_file = open('data/Assembly.fasta', 'w')
+    
+    for i in range(len(seqs)) :
+        name = '>sequence'+str(i)
+        fasta_file.write(name + '\n')
+        fasta_file.write(seqs[i]+'\n')
+            
+    print(len(seqs))
+    print(len(r))
+    print(time.time()-t1)
+    
+    return seqs
+
+#Return a list in which each element contains a list of linked contigs (accroding to GFA). There is one list for each end of the contig
+def gfa_to_python(lenseqs): #algo marche si les noms des séquences sont leur indice *2
+    
+    gfa_read = open('Assembly.gfa')
+    r = gfa_read.read()
+    
+    count = 0
+    digits = ['0','1','2','3','4','5','6','7','8','9']
+    orientation = ['-','+']
+    
+    links = [[] for i in range(lenseqs*2)] #a list of links at one end of a contig
+    number = ''
+    n1 = -1 #integers that refer to the two contigs of interest
+    n2 = -1
+    n1orientation = -1 # refer to the orientation of the contig (0 if -, 1 if +)
+    n2orientation = -1
+    link = False
+        
+    for i in range(len(r)) :
+        count += 1
+        if r[i] == 'L' :
+            link = True
+        
+        if link == True :
+            
+            if r[i] in digits :
+                number += r[i]
+                
+            if r[i] in orientation :
+                
+                n = int(number)
+                number = ''
+                
+                if n1 == -1 :
+                    n1 = n
+                    if r[i] == '-' :
+                        n1orientation = 0
+                    else :
+                        n1orientation = 1
+                    
+                else :
+                    n2 = n
+                    if r[i] == '-' :
+                        n2orientation = 0
+                    else :
+                        n2orientation = 1
+                    
+                    links[int(n1 + n1orientation)] += [int(n2 + 1 - n2orientation)]
+                    links[int(n2 + 1 - n2orientation)] += [int(n1 + n1orientation)]
+                    
+                    #resetting all the parameters
+                    n1 = -1
+                    n2 = -1
+                    n1orientation = -1
+                    n2orientation = -1
+                    link = False
+            
+    return links
+
+
+def print_short():
+    
+    gfa_read = open('data/Assembly.gfa')
+    r = gfa_read.read()
+    
+    bases = ['A','C','G','T']
+    count = 0
+    s = ''
+    
+    for i in range(len(r)) :
+        if r[i] in bases :
+            if count%500000000==0 :
+                s+= r[i]
+        else :
+            s+=r[i]
+        count+=1
+    print (s)
+    return 0
+
+#print_short()
+        
+
+#a function to test that our gfa_to_python worked ok
+def check_links(links):
+    for i, noeud in enumerate(links) :
+        for j, neighbor in enumerate(noeud):
+            found = False
+            for k in links[neighbor] :
+                if k == i :
+                    found = True
+            if not found :
+                print('Problem : ', i, neighbor)
+                
