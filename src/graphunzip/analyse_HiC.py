b3d8a30ae9097b8a2c1ec5333627a6d57a0b9f50
Adam Taranto <adam.p.taranto@gmail.com>
2024-01-08 16:50:54 +1100

    Fix module paths

--- src/graphunzip/analyse_HiC.py
+++ src/graphunzip/analyse_HiC.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import random
-from transform_gfa import load_gfa
-import basic_functions as bf
+from graphunzip.transform_gfa import load_gfa
+
+# import basic_functions as bf
 
 
 def short_distance_interactions(fragcontacts, fraglist):
-
     scores = []
     scoreFar = []
     for i in fragcontacts:
@@ -25,27 +25,31 @@ def short_distance_interactions(fragcontacts, fraglist):
     plt.ylim([0, 1000])
     plt.show()
 
-def distance_law(hiccontactsfile, fragmentList, header = True):
+
+def distance_law(hiccontactsfile, fragmentList, header=True):
     tableDistance = (
         []
     )  # we're going to visualize distance law by looking at the inside of contigs
     tableIntensity = []
-    
-    f = open(hiccontactsfile,'r')
-    
+
+    f = open(hiccontactsfile, "r")
+
     for line in f:
-        if not header :
-            l = line.strip('\n').split('\t')
+        if not header:
+            l = line.strip("\n").split("\t")
             i = [int(l[0]), int(l[1]), int(l[2])]
-            if (fragmentList[i[0]][0] == fragmentList[i[1]][0] and i[2] > 0 ):  # i.e. we are in the same contig
-    
+            if (
+                fragmentList[i[0]][0] == fragmentList[i[1]][0] and i[2] > 0
+            ):  # i.e. we are in the same contig
                 distance = fragmentList[i[1]][1] - fragmentList[i[0]][1]
                 tableDistance += [distance]
-                tableIntensity += [i[2]] #division to normalize to the probability of interaction per bp
-                #/ fragmentList[i[0]][3] / fragmentList[i[1]][3]
-        else :
+                tableIntensity += [
+                    i[2]
+                ]  # division to normalize to the probability of interaction per bp
+                # / fragmentList[i[0]][3] / fragmentList[i[1]][3]
+        else:
             header = False
-            
+
     plt.scatter(tableDistance, tableIntensity, alpha=0.2)
     plt.xlabel("Distance (bp)")
     plt.ylabel("Intensity of contact")
@@ -53,17 +57,16 @@ def distance_law(hiccontactsfile, fragmentList, header = True):
     plt.ylim([0, 25])
     plt.show()
 
+
 #    bf.export_to_csv([tableDistance, tableIntensity], 'listsPython/distanceIntensite.csv')
 
+
 def with_how_many_contig_does_one_contig_interact(hiccontactsfile, fragmentList):
-
     with open(hiccontactsfile) as f:
-
         what_contigs_interact_with_this_one = [
             [] for i in range(fragmentList[-1][0] + 1)
         ]
         for line in f:
-
             line = line.strip("\n")
             line = line.split("\t")
 
@@ -73,7 +76,6 @@ def with_how_many_contig_does_one_contig_interact(hiccontactsfile, fragmentList)
                 if (
                     contact[2] > 4
                 ):  # this diminishes a lot the number of contig one contig interacts with, probably filtering out errors and rare events
-
                     contig1 = fragmentList[contact[0]][0]
                     contig2 = fragmentList[contact[1]][0]
 
@@ -93,14 +95,13 @@ def with_how_many_contig_does_one_contig_interact(hiccontactsfile, fragmentList)
         plt.ylabel("Number of contig interacting with x others")
         plt.show()
 
+
 # breaking down contigs to see how much HiC contact have fragments that are actually touching
 def testHiC_vs_GFA(hiccontacts, info_contigs):
-
     contactNumber = 0
     score = []
 
     for contig in range(len(info_contigs)):
-
         start_frag = info_contigs[contig][3]
         end_frag = info_contigs[contig][3] + info_contigs[contig][2]
 
@@ -115,7 +116,6 @@ def testHiC_vs_GFA(hiccontacts, info_contigs):
                 print(contig, start_frag, end_frag, cut)
 
             while hiccontacts[contactNumber][0] < start_frag + cut:
-
                 if (
                     hiccontacts[contactNumber][0] > start_frag
                     and hiccontacts[contactNumber][1] >= start_frag + cut
@@ -135,11 +135,11 @@ def testHiC_vs_GFA(hiccontacts, info_contigs):
     plt.hist(score)
 
 
-#hiccontacts = bf.read_abs_fragments_contact_weighted('data/results/abs_fragments_contacts_weighted.txt')
+# hiccontacts = bf.read_abs_fragments_contact_weighted('data/results/abs_fragments_contacts_weighted.txt')
 # hiccontacts = import_from_csv('listsPython/hiccontacts.csv')
 # print(hiccontacts[:20])
 # print(hiccontacts[:100])
-#fragmentList = bf.read_fragment_list("data/results/fragments_list.txt")
+# fragmentList = bf.read_fragment_list("data/results/fragments_list.txt")
 # print(fragmentList[:100])
 # infcontigs = read_info_contig('data/results/info_contigs.txt')
 # links = gfa_to_python(1312)
@@ -175,10 +175,9 @@ def testHiC_vs_GFA(hiccontacts, info_contigs):
 # )
 # im = sp.lil_matrix(interaction_Matrix)
 # pickle.dump(im, "listsPython/interactionMatrix.pickle")
-# bf.export_to_csv(interaction_Matrix, "listsPython/interactionMatrix.csv") 
+# bf.export_to_csv(interaction_Matrix, "listsPython/interactionMatrix.csv")
 # print(interaction_Matrix[217][323], interaction_Matrix[217][359])
 
-#distance_law('data/results/abs_fragments_contacts_weighted.txt', fragmentList)
+# distance_law('data/results/abs_fragments_contacts_weighted.txt', fragmentList)
 
-#print("Finished")
-
+# print("Finished")
